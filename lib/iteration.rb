97ebbbea9df4369cd4988ebd4d63a0c4d69dfb2d
Gustave Habimana <ghabiman@gmail.com>
2020-02-14 21:26:06 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -42,9 +42,13 @@ def total_even_pairs(src)
   new_Array = []
   index = 0 
   
-  while row_index < src.count do 
-    if src [index] [0] % 2 == 0 && src [row_index][1] % 2 == 0
-      total += src[row_index][0] + src [row_index][1]
+  while index < src.count do 
+    if src [index] [0] % 2 == 0 && src [index][1] % 2 == 0
+      new_Array += src[index][0] + src [index][1]
   end
-  row_index += 1
+  index += 1
 end
+total
+
+
+
