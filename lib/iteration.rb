397f6f5f0cf1bf3a3bdf810c64c541858f1e5abb
Gustave Habimana <ghabiman@gmail.com>
2020-02-14 20:42:43 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -20,6 +20,9 @@ def find_greater_pair(src)
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
   # Produce a new Array that contains the larger number of each of the pairs
   # that are in the inner Arrays
+  new_Array = []
+  index = 0 
+  
   
 end
 
