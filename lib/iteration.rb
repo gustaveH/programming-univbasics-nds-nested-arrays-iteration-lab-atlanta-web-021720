9b6df319cc5950e8c87889649aa5fe756c4594df
Gustave Habimana <ghabiman@gmail.com>
2020-02-14 21:01:21 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -21,11 +21,6 @@ def find_greater_pair(src)
   # Produce a new Array that contains the larger number of each of the pairs
   # that are in the inner Arrays
   new_Array = []
-  index = 0 
-  while index < src.count do 
-    new_Array << src.index.rand 
-    index = index +1
-  end
   
 end
 
