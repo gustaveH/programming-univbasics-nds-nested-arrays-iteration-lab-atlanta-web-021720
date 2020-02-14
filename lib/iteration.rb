835f08c53ae25862ded3fd5c1283dab62b8a023b
Gustave Habimana <ghabiman@gmail.com>
2020-02-14 21:17:25 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -23,7 +23,7 @@ def find_greater_pair(src)
   new_Array = []
   index = 0 
   while index < src.count do
-    new_Array << index += 1
+    new_Array[index]= src[undex].max
     index = index +1
   end
   return new_Array
